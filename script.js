62870a635e01b2a63c30b17aec66e6ba62c333ee
dawe124 <kovacs.david.istvan@gmail.com>
2022-06-08 16:03:50 +0700

    talk animation basics added

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-let buttons = document.querySelectorAll('.menu-item')
+const buttons = document.querySelectorAll('.menu-item')
+const fish = document.querySelectorAll('.fish')[0]
 
 buttons.forEach(btn =>
     btn.addEventListener('click', (e) => {
         e.preventDefault()
         removeActive()
         e.target.parentElement.classList.add('active')
+        console.log(e.target.dataset.id)
+        fish.addEventListener('animationiteration', () => {
+            console.log('animation ended')
+            fish.className = 'fish'
+            fish.classList.add(actionList[e.target.dataset.id])
+        })
     })
 )
 
@@ -12,4 +19,13 @@ const removeActive = () => {
     buttons.forEach(btn => {
         btn.classList.remove('active')
     })
+}
+
+const actionList = {
+    'idle': 'idle-body',
+    'swim': 'swim-body',
+    'flip': 'flip-body',
+    'talk': 'talk-body',
+    'sleep': 'sleep-body',
+    'die': 'die-body'
 }
